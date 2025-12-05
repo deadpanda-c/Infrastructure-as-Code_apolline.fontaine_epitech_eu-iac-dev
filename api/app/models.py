09250b2ee36ac6019cf2d574f32c9f36d5f904bf
Clément Lagier <clement.lagier@epitech.eu>
2025-12-05 16:45:12 +0100

    [ADD] Added an User table within models.py

--- api/app/models.py
+++ api/app/models.py
@@ -3,3 +3,20 @@ from database import Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import ForeignKey
 
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), unique=True)
+    email = Column(String(50), unique=True)
+    password = Column(String(50))
+    admin = Column(Boolean)
+    
+    def __init__(self, email, username, password, is_admin):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.admin = is_admin
+        
+    def __repr__(self):
+        return '<User %r>' % self.username
+
