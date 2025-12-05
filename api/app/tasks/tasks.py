e605fa876a460d7064aeb6fbb64117beb2b89842
Clément Lagier <clement.lagier@epitech.eu>
2025-12-05 17:00:16 +0100

    [ADD] Added basic CRUD (no databases linked, just the function are written

--- api/app/tasks/tasks.py
+++ api/app/tasks/tasks.py
@@ -13,8 +13,22 @@ router = APIRouter(
 )
 
 
-@router.get("/test")
-def test():
-    return {"test": "test"}
+@router.get("/")
+def read_tasks():
+  return {"tasks": ["task1", "task2"]}
 
+@router.get("/{task_id}")
+def read_task(task_id: int, db: Session = Depends(get_db)):
+  return {"task": f"task {task_id}"}
 
+@router.post("/")
+def create_task():
+  return {"task": "task created"}
+
+@router.put("/{task_id}")
+def update_task():
+  return {"task": "task updated"}
+
+@router.delete("/{task_id}")
+def delete_task():
+  return {"task": "task deleted"}
